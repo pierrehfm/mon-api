581f2b43edd3782c55157033d6e25ed9a5f74e0b
Pierre Hofmann <pierreh2004@gmail.com>
2026-03-02 14:03:59 +0100

    add lint

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// eslint.config.js
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "semi": ["error", "always"],
+      "no-console": "off",
+    },
+  },
+];
